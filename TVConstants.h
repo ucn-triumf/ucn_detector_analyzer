1d370148cfff3867c0b9e05f5c00fd25b7632403
Thomas Lindner <lindner@triumf.ca>
2017-10-21 22:59:57 -0700

    Missed this file.

---
+++ TVConstants.h
@@ -0,0 +1,15 @@
+#ifndef TVConstants_h_
+#define TVConstants_h_
+
+#ifndef PSD_MAXNCHAN
+#define PSD_MAXNCHAN  8
+#endif
+#ifndef NDPPBOARDS
+#define  NDPPBOARDS 2
+#endif
+
+#ifndef HVCHANNELS
+#define  HVCHANNELS 24
+#endif
+
+#endif
