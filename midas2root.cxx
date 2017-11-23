dee33ad165b34b93b93f3e4a97a0290e04ff57f6
Thomas Lindner <lindner@triumf.ca>
2017-11-22 20:34:34 -0800

    Adding new tree with header information; run comment and shifter

--- midas2root.cxx
+++ midas2root.cxx
@@ -7,6 +7,7 @@
 #include "TRootanaEventLoop.hxx"
 #include "TAnaManager.hxx"
 #include "TUCNAnaViewer3.h"
+#include "XmlOdb.h"
 
 class Analyzer: public TRootanaEventLoop {
 
@@ -63,6 +64,20 @@ public:
     
     InitManager();
     anaManager->BeginRun(transition, run, time);
+
+    // Get the run comment and the shifter from the ODB dump at BOR.
+    XmlOdb *odb = dynamic_cast<XmlOdb*>(GetODB());
+    TTree *headerTree = new TTree("headerTree", "headerTree");
+
+    std::string shifter_string(odb->odbReadString("/Experiment/Edit on start/Shifters",0,"XXX"));
+    std::string comment_string(odb->odbReadString("/Experiment/Edit on start/Comment",0,"XXX"));
+    headerTree->Branch("shifter", &shifter_string);
+    headerTree->Branch("comment", &comment_string);
+    headerTree->Fill();
+ 
+    std::cout << "Run shifter " << shifter_string << std::endl;
+    std::cout << "Run comment " << comment_string << std::endl;
+    
     
   }
 
