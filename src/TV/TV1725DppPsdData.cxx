4995117e885c9783f0996d0e6a5054d27aee85ae
Thomas Lindner <lindner@triumf.ca>
2025-08-07 07:16:54 -0700

    Change corrects the decoding of the V1725 bank.  Specifically it adds the proper support for having multiple hits per bank.

--- src/TV/TV1725DppPsdData.cxx
+++ src/TV/TV1725DppPsdData.cxx
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include <iostream>
+#include <iomanip>
 
 
 TV1725DppPsdData::TV1725DppPsdData(int bklen, int bktype, const char* name, void *pdata):
@@ -13,6 +15,8 @@ TV1725DppPsdData::TV1725DppPsdData(int bklen, int bktype, const char* name, void
   // Do decoding.  Decoding is complicated by the fact that there can be 
   // multiple events in the same bank.  So need to find and save multiple
   // event headers, trailers.
+
+
 
   fGlobalHeader.push_back(GetData32()[0]);
   fGlobalHeader.push_back(GetData32()[1]);
@@ -27,7 +31,30 @@ TV1725DppPsdData::TV1725DppPsdData(int bklen, int bktype, const char* name, void
 	      << std::hex << GetData32()[0] << std::dec << std::endl;
   
   int counter = 4;
+
+
+  //std::cout << "TV1725 add " << std::endl;
   
+
+  // std::cout << "Board Agg size " << (GetData32()[0] & 0xfffffff) 
+  //	    << " bklen: " << bklen << std::endl;
+  //  std::cout << std::hex << "0x" << GetData32()[0] << std::dec << std::endl;
+  //std::cout << std::hex << " channel mask:" << GetChMask() << std::dec << std::endl;
+
+
+
+  if(0) for (int i = 0; i < 128; i++){
+    std::cout << std::hex << std::setw(8) <<  std::setfill('0') << GetData32()[i] << " ";
+    if (i%8 == 7) std::cout << std::dec << std::endl;
+
+
+  }
+  //  std::cout << std::dec << std::endl;
+
+
+
+
+
 
   // Loop over dual channel data
   for(int ch = 0; ch < 8; ch++){
@@ -36,42 +63,66 @@ TV1725DppPsdData::TV1725DppPsdData(int bklen, int bktype, const char* name, void
       
       
       uint32_t header0 = GetData32()[counter];
+      uint32_t size = header0 & 0x3fffff;
+
+      uint32_t ch_agg_size = size ; // calculate number of samples.
+      //      std::cout << "Have data for ch group=" << ch << "  with ch_agg_size " << ch_agg_size << std::endl;
+      // Let's check if the next channel aggregate is in the right place
+      //for(int i = 0; i < 9; i++){
+      //	std::cout << std::hex << GetData32()[counter+ch_agg_size+i]  << std::dec << std::endl;
+      //}
       counter++;
+
       uint32_t header1 = GetData32()[counter];
       counter++;
-      uint32_t header2 = GetData32()[counter];
-      counter++;
-      uint32_t size = header0 & 0x7fff;
 
-      ChannelMeasurement meas = ChannelMeasurement(ch,header0,header1,header2);
+      uint32_t n_samples_d8 = header1 & 0xffff ; // calculate number of samples.
+      //      std::cout << "n_samples_d8 " << n_samples_d8 << std::endl;
       
-      int nsamples = size - 5; // calculate number of samples.
-      
-      std::vector<uint32_t> Samples;
-      for(int i = 0; i < nsamples; i++){
-	uint32_t sample = (GetData32()[counter] & 0x3fff);
-	Samples.push_back(sample);
-	sample = (GetData32()[counter] & 0x3fff0000) >> 16;
-	Samples.push_back(sample);				
+      int total_size_ch_agg = n_samples_d8*4 + 3;
+      int total_events = (ch_agg_size-2)/total_size_ch_agg;
+      //std::cout << "total_size_ch_agg : " << total_size_ch_agg << std::endl;
+      //  std::cout << "total number of events in channel aggregate: " << total_events << std::endl;
+
+      for(int evt = 0; evt < total_events; evt++){
+
+	uint32_t header2 = GetData32()[counter];
 	counter++;
+	ChannelMeasurement meas = ChannelMeasurement(ch,header0,header1,header2);
+
+
+	std::vector<uint32_t> Samples;
+	for(int i = 0; i < n_samples_d8*4; i++){
+	  uint32_t sample = (GetData32()[counter] & 0x3fff);
+	  Samples.push_back(sample);
+	  sample = (GetData32()[counter] & 0x3fff0000) >> 16;
+	  Samples.push_back(sample);				
+	  counter++;
+	}
+	meas.AddSamples(Samples);
+	
+	uint32_t extras = GetData32()[counter];
+	counter++;
+	meas.AddExtra(extras);
+	uint32_t qs = GetData32()[counter];
+	counter++;
+	meas.AddQs(qs);
+      
+	if(Samples.size() != (0xffff & header1 )*8){
+	  
+	  std::cout << "Check2: " << Samples.size() << " " << (0xffff & header1 )*8 
+		    << " whoops, mistake in decoding, sample size not as expected. counter="
+		    << counter << std::endl;
+	}
+	
+	fMeasurements.push_back(meas);
+      
       }
-      meas.AddSamples(Samples);
-
-      uint32_t extras = GetData32()[counter];
-      counter++;
-      meas.AddExtra(extras);
-      uint32_t qs = GetData32()[counter];
-      counter++;
-      meas.AddQs(qs);
-      
-      if(Samples.size() != (0xffff & header1 )*8)
-	std::cout << "Check2: " << Samples.size() << " " << (0xffff & header1 )*8 
-		  << " whoops, mistake in decoding, sample size not as expected. " << std::endl;
-
-      fMeasurements.push_back(meas);
-      
     }
   }
+
+  //  std::cout << "Finished looping over many! " << counter << " total measurements: " << fMeasurements.size() << std::endl;
+
   //std::cout << "Number of measurements: " << fMeasurements.size() << std::endl;
   if(GetChMask() == 1999999){
     std::cout << "----------------------" << std::endl;
@@ -82,7 +133,7 @@ TV1725DppPsdData::TV1725DppPsdData(int bklen, int bktype, const char* name, void
   }
 
   // Check that the final counter seems to match the expected aggregate size.
-  if((unsigned int)counter != (0xfffffff & GetData32()[0])){
+  if(0 && (unsigned int)counter != (0xfffffff & GetData32()[0])){
     std::cout << "_________________________________________________________" << std::endl;
     std::cout << "_________________________________________________________" << std::endl;
     std::cout << "Check:  " << counter << " " << (0xfffffff & GetData32()[0]) 
